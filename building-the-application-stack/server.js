0d2d19296f00e0b9b181879d636c4e7e1b3f63c5
Philip Alegret <philip.alegret@gmail.com>
2016-06-17 07:26:44 -0400

    Added logging

--- building-the-application-stack/server.js
+++ building-the-application-stack/server.js
@@ -1,11 +1,15 @@
+var PORT = 8888;
+
 var http = require("http");
 var requestListener = function(request, response) {
+    console.log("Request received.");
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.write("Hello World");
     response.end();
 };
 
-http.createServer(requestListener).listen(8888);
+http.createServer(requestListener).listen(PORT);
+console.log("Server has started on port " + PORT + ".");
 
 // More info:
 // http://debuggable.com/posts/understanding-node-js:4bd98440-45e4-4a9a-8ef7-0f7ecbdd56cb
