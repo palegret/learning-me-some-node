64cf6ee17094b0a0574a5216aba5570e5cf8e566
Philip Alegret <philip.alegret@gmail.com>
2016-06-17 09:59:19 -0400

    Added and implemented routing module
    Description added from command line.

---
+++ building-the-application-stack/router.js
@@ -0,0 +1,10 @@
+"use strict";
+
+function route(pathname) {
+    console.log(`About to route a request for ${pathname}.`);
+}
+
+exports.route = route;
+
+// More info:
+// http://martinfowler.com/articles/injection.html
